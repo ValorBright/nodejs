b418554ad8dc6fce47c87be599879837a1abd394
ValorBright <valorauberonbright@gmail.com>
2021-02-17 11:28:22 -0500

    Created more stuff

---
+++ comparingnumbers.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let daysInWeek = 7;
+
+console.log("daysInWeek == 6", daysInWeek == 6);    //Equality Test
+console.log("daysInWeek === 6", daysInWeek === 6);  //Identity Test
+console.log("daysInWeek != 6", daysInWeek != 6);    // Not Equal
+console.log("daysInWeek !=== 6", daysInWeek !== 6); // Not identical
+console.log("daysInWeek < 8", daysInWeek < 8);      // Less than
+console.log("daysInWeek > 8", daysInWeek > 8);      // Greater than
+console.log("daysInWeek <= 7", daysInWeek <= 7);    // Less than or equal to
+console.log("daysInWeek >= 8", daysInWeek >= 8);    // Great than or equal to
+
---
+++ conditions.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let temperature = process.argv[2];
+
+if (temperature < 20.0) {
+    console.log("Activating heating");
+}
+else if (temperature > 35.0) {
+    console.log("Room is overheating! Highway to the Danger Zone!");
+}
+else if (temperature > 21.0) {
+    console.log("De-activating heating");
+}
+
+else {
+    console.log("Temperature is normal.");
+}
+
